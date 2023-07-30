c65ced21fa0bd57f440e0d768ca5d71fb9dd88d0
sayog donga <sayogdonga156156@gmail.com>
2023-07-30 18:33:06 +0530

    second change

--- config/connectDb.js
+++ config/connectDb.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const colors = require("colors");
 const connectDb = async () => {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/expense-management');
+    await mongoose.connect('mongodb+srv://sayogdonga156156:4rxwMVKZ0iLvSKRV@cluster0.fr3ikpp.mongodb.net/expense-management');
     console.log(`Connected Successfully`);
   } catch (error) {
     console.log(`${error}`.bgRed);
