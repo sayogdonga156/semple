f824d0aae87d11636485dee224b1e9b0ef371d1d
sayog donga <sayogdonga156156@gmail.com>
2023-07-30 18:40:30 +0530

    index

--- server.js
+++ server.js
@@ -28,9 +28,6 @@ app.use("/api/v1/transections", require("./routes/transectionRoutes"));
 //static files
 app.use(express.static(path.join(__dirname, "./client/build")));
 
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
 
 //port
 const PORT = 8080 || process.env.PORT;
